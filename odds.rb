a1b6fe66ea299c23ae57c539097750606f2fc5cb
costagavras <cgavras@yahoo.ca>
2018-12-13 17:58:28 -0500

    odds

---
+++ odds.rb
@@ -0,0 +1,23 @@
+i_dice_1 = 1
+i_dice_2 = 1
+i_dice_total = 0
+hh_times = {}
+
+#initialize the hash
+(2..12).each do |sum|
+  hh_times[sum] = 0
+end
+
+6.times do
+  6.times do
+    hh_times[i_dice_1 + i_dice_2] += 1
+    i_dice_2 += 1
+    i_dice_total += 1
+  end
+  i_dice_1 += 1
+  i_dice_2 = 1
+end
+
+hh_times.each do |k, v|
+  puts "The odds of #{k} coming up are #{(v.to_f / i_dice_total * 100).round}%"
+end
